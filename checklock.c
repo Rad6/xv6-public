0698b0b82be73c27079dc27f942d394e093b102f
plutocholia <kiarash.nrz77@gmail.com>
2020-05-28 16:58:19 +0430

    user program of checklock is completed

--- checklock.c
+++ checklock.c
@@ -5,10 +5,28 @@
 int
 main(int argc, char *argv[])
 {
-  if(argc > 1){
-    printf(2, "checklock: no need to add any args\n");
+  if(argc < 2){
+    printf(2, "checklock: enter the number of processes u want.\n");
     exit();
   }
-  
+  int nof_proc = atoi(argv[1]);
+  int is_dady = 1;
+  for(int i = 0; i < nof_proc; i++){
+    int fk = fork();
+    if(fk == 0){
+      is_dady = 0;
+      check_lock();
+      break;
+    }
+    else{
+      if(i == 0)
+      check_lock();
+    }
+  }
+
+  if(is_dady){
+    for(int i = 0; i < nof_proc; i++)
+      wait();
+  }
   exit();
 }
